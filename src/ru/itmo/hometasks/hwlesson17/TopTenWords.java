6fecb1699d49b8b3db98115a851a01a08890d58f
Егор Герасимов <horrowglack@gmail.com>
2022-04-13 00:01:56 +0300

    Домашняя работа по занятию №17

---
+++ src/ru/itmo/hometasks/hwlesson17/TopTenWords.java
@@ -0,0 +1,27 @@
+package ru.itmo.hometasks.hwlesson17;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TopTenWords {
+    public static void main(String[] args) {
+
+        String text = "It is a uncover long established fact that a reader will be distracted uncover by the readable content of a page " +
+                "when looking at its layout the point of using uncover Lorem Ipsum is that sites it has a more-or-less normal distribution of letters" +
+                "as uncover opposed to still using Content here humour uncover content here making it look like readable English Many desktop publishing " +
+                "packages and web page editors now use Lorem Ipsum as their default model text and a search for lorem ipsum will " +
+                "uncover many web sites still uncover in their infancy Various versions uncover have evolved over the years uncover sometimes by accident" +
+                " sometimes on purpose injected humour and the like";
+
+        // TODO: Используя Stream API: создать Map<String, Long>, где
+        //  - String - слово
+        //  - Long - частота встречаемости
+        //  В мапу должны войти только первые 10 по частоте встречаемости слов.
+
+        List<String> listOfWords = new ArrayList<>(Arrays.asList(text.split(" ")));
+        //Map<String, Object> mapOfWords =listOfWords.stream().collect(Collectors.toMap(Function.identity(), elem->Collections.frequency(listOfWords,elem),(elem1,elem2)->elem1));
+        Map<String, Long> mapOfWords =listOfWords.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        mapOfWords.entrySet().stream().sorted(Map.Entry.<String,Long>comparingByValue().reversed()).limit(10).forEach(System.out::println);
+    }
+}
